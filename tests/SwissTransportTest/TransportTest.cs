97e1161809e29b7f76c6e4cc2046bc87ade35720
Lars Ghezzi <l.ghezzi@bluewin.ch>
2018-12-03 11:33:06 +0100

    add Date and Time to Connenction in API

--- tests/SwissTransportTest/TransportTest.cs
+++ tests/SwissTransportTest/TransportTest.cs
@@ -29,7 +29,7 @@ namespace SwissTransport
         public void Connections()
         {
             testee = new Transport();
-            var connections = testee.GetConnections("Sursee", "Luzern");
+            var connections = testee.GetConnections("Sursee", "Luzern", "2018-12-13", "16:30");
 
             Assert.IsNotNull(connections);
         }
