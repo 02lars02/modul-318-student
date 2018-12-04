551a09540e617bdc2bf9b5fd3da142e0d47f658e
Lars Ghezzi <l.ghezzi@bluewin.ch>
2018-12-04 10:14:31 +0100

    disable MaximizeButton

--- TransportApp/GUI.Designer.cs
+++ TransportApp/GUI.Designer.cs
@@ -156,6 +156,7 @@
             this.Controls.Add(this.btnConnectionboard);
             this.Controls.Add(this.btnConnection);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "GUI";
             this.Text = "Transport App";
             this.ResumeLayout(false);
